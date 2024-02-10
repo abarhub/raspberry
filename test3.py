26a1f3bea19474d401d0255127fa3eda417ffb99
abarhub <abarhub@gmail.com>
2024-02-10 10:34:25 +0100

    correction des led tricolors

--- test3.py
+++ test3.py
@@ -1,17 +1,3 @@
-from gpiozero import LED
-from time import sleep
-
-
-
-led = LED(17)
-
-while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
-	
-	
 from gpiozero import Button,LED
 from time import sleep
 
